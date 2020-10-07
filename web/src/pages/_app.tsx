bf639c17f1928f13f60b061d25aa74ca1a200dce
hylickipiotr <pietrek0212@gmail.com>
2020-10-07 11:27:01 +0200

    Added initFontAwesome

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import React from "react";
 import { AppProps } from "next/app";
-
 import "../styles/tailwind.css";
-import { withApollo } from "../utils/withApollo";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { initFontAwesome } from "../utils/initFontAwesome";
 
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
-  credentials: "include",
-  cache: new InMemoryCache(),
-});
+initFontAwesome();
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
---
+++ web/src/utils/initFontAwesome.tsx
@@ -0,0 +1,6 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+export const initFontAwesome = () => {
+  library.add(fab);
+};
