cfa355447bda779a4ac186f59a1101fa3742a4bb
hylickipiotr <pietrek0212@gmail.com>
2020-10-12 11:44:13 +0200

    Set video thumbnail fixed aspect ratio

--- web/src/components/VideoCard/VideoCard.tsx
+++ web/src/components/VideoCard/VideoCard.tsx
@@ -33,12 +33,14 @@ const VideoCard: React.FC<IVideoCard> = ({
         <div className="absolute flex justify-center items-center w-full h-full bg-gray-800 bg-opacity-25 hover:bg-opacity-0 transition-all ease-in-out duration-200 group">
           <PlayIcon className="w-12 h-12 fill-current text-white" />
         </div>
-        <img
-          className="w-full md:h-40 lg:h-56 object-cover"
-          src={thumbnailUrl}
-          alt={title}
-          title={title}
-        />
+        <div className="relative w-full pb-7/12">
+          <img
+            className="absolute w-full h-full object-cover"
+            src={thumbnailUrl}
+            alt={title}
+            title={title}
+          />
+        </div>
       </div>
       <p className="mt-4 text-xl font-semibold">{title}</p>
       <p className="mt-1 text-sm font-secondary leading-6 text-gray-700">
