8cb822193709b119c6c6dd6031738401644f9f11
hylickipiotr <hylickipiotr@gmail.com>
2020-11-19 17:24:32 +0100

    Fixed player sizing

--- web/src/components/VideoPlayer/VideoPlayer.tsx
+++ web/src/components/VideoPlayer/VideoPlayer.tsx
@@ -1,19 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import ReactPlayer from "react-player";
+import { useWindowSize } from "../../utils/useWindowSize";
 import { usePlayerContext } from "./PlayerContext";
 
+type ElementSize = {
+  width: number;
+  height: number;
+};
 interface IVideoPlayer {
   url: string;
 }
 
+const RATIO = 1280 / 720;
+const REM = parseFloat(
+  window.getComputedStyle(document.documentElement).fontSize
+);
+const PADDING_VIDEO = 4 * REM;
+
 const VideoPlayer: React.FC<IVideoPlayer> = ({ url }) => {
+  const windowSize = useWindowSize();
   const { isOpen, setIsOpen } = usePlayerContext();
+  const playerContainer = useRef<HTMLDivElement>(null);
+  const [containerSize, setContainerSize] = useState<ElementSize>({
+    width: 0,
+    height: 0,
+  });
+
+  const tempPlayerSize: ElementSize = {
+    width: containerSize.width,
+    height: containerSize.width / RATIO,
+  };
+  const playerSize =
+    tempPlayerSize.height > containerSize.height
+      ? {
+          width: containerSize.height * RATIO,
+          height: containerSize.height,
+        }
+      : tempPlayerSize;
+
+  useEffect(() => {
+    const { current } = playerContainer;
+    if (!current) return;
+    setContainerSize({
+      width: current.clientWidth - PADDING_VIDEO * 2,
+      height: current.clientHeight - PADDING_VIDEO * 2,
+    });
+  }, [windowSize, playerContainer, isOpen]);
 
   if (!isOpen) {
     return null;
   }
-
   return (
     <div
       className={`${
@@ -28,12 +65,17 @@ const VideoPlayer: React.FC<IVideoPlayer> = ({ url }) => {
           onClick={() => setIsOpen(false)}
         />
       </div>
-      <div className="h-9/12 relative w-full flex justify-center items-center p-2">
+      <div
+        className="flex w-full h-full items-center justify-center"
+        ref={playerContainer}
+      >
         <ReactPlayer
-          style={{ maxWidth: "100%", maxHeight: "100%" }}
+          className="react-player"
           url={url}
           controls={true}
           playing={true}
+          width={playerSize.width}
+          height={playerSize.height}
         />
       </div>
     </div>
--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,12 +4,9 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { ToastProvider } from "react-toast-notifications";
 import PageLoader from "../components/PageLoader/PageLoader";
 import "../styles/tailwind.css";
+import "../styles/main.css";
 import { initFontAwesome } from "../utils/initFontAwesome";
 import { useRouterLoading } from "../utils/useRouterLoading";
-import { Router } from "next/router";
-import { pageview } from "../utils/gtag";
-
-Router.events.on("routeChangeComplete", (url) => pageview(url));
 
 initFontAwesome();
 
