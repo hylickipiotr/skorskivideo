8b4322e7f660b8e2197808921a280618cdd21697
hylickipiotr <hylickipiotr@gmail.com>
2020-11-19 17:24:56 +0100

    Added tailwind purge

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: ["./**/{pages,components}/**/*.{js,jsx,ts,tsx}"],
   target: "relaxed",
   prefix: "",
   important: false,
